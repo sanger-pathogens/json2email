832cd2610307fab015c767a6fdffd1bb7c24e5e7
Ben Taylor <bewt85@gmail.com>
2015-03-24 13:28:33 +0000

    Add reasons to Email failures

--- smtplibwrapper/__init__.py
+++ smtplibwrapper/__init__.py
@@ -1,1 +1,18 @@
-from .Email import Email
+import re
+
+class Email(object):
+  def __init__(self, template, data):
+    self.template = template
+    self.data = data
+
+  def should_send(self):
+    just_whitespace = re.compile('^\s*$')
+    try:
+      output = self.template.render(self.data)
+    except:
+      return (False, "Couldn't render email")
+    matches = just_whitespace.match(output)
+    if matches == None:
+      return (True, "Seems OK to send")
+    else:
+      return (False, "Email was empty")
--- smtplibwrapper/tests/Email_test.py
+++ smtplibwrapper/tests/Email_test.py
@@ -8,15 +8,19 @@ class TestEmail(unittest.TestCase):
     template = MagicMock()
     email = Email(template=template, data={})
 
+    empty_template_response = (False, "Email was empty")
+    render_failure_response = (False, "Couldn't render email")
+    ok_response = (True, "Seems OK to send")
+
     negative_render_return_cases = ['', ' ', ' \n ', ' \t ', ' \n\t ']
     for test_case in negative_render_return_cases:
       email.template.render.return_value = test_case
-      self.assertFalse(email.should_send(), "Should not send if rendered output is '%s'" % test_case)
+      self.assertEqual(email.should_send(), empty_template_response, "Should not send if rendered output is '%s'" % test_case)
 
     positive_render_return_cases = ['foo', '\nfoo']
     for test_case in positive_render_return_cases :
       email.template.render.return_value = test_case
-      self.assertTrue(email.should_send(), "Should send if rendered output is '%s'" % test_case)
+      self.assertEqual(email.should_send(), ok_response, "Should send if rendered output is '%s'" % test_case)
 
     email.template.render.side_effect = Exception("Fake template rendering exception")
-    self.assertFalse(email.should_send(), "Should not send if rendering raises an Exception")
+    self.assertEqual(email.should_send(), render_failure_response, "Should not send if rendering raises an Exception")
