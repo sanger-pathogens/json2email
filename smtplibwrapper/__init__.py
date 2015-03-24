93938f055245d2af4588483432e46e87738b869a
Ben Taylor <bewt85@gmail.com>
2015-03-24 13:29:13 +0000

    Add email server object

--- smtplibwrapper/__init__.py
+++ smtplibwrapper/__init__.py
@@ -1,4 +1,27 @@
 import re
+import smtplib
+
+class EmailServer(object):
+  def __init__(self, *args, **kwargs):
+    self.server = smtplib.SMTP(*args, **kwargs)
+
+  def __del__(self):
+    self.server.quit()
+
+  def send(self, email):
+    (should_send, reason) = email.should_send()
+    if not should_send:
+      return (False, reason)
+    else:
+      sender = email.sender
+      recipients = email.recipients
+      try:
+        email_content = email.to_string()
+        self.server.sendmail(sender, recipients, email_content)
+        self.server.foo()
+      except Exception:
+        return (False, "Problem sending the email")
+      return (True, "Sent OK")
 
 class Email(object):
   def __init__(self, template, data):
--- smtplibwrapper/tests/Email_test.py
+++ smtplibwrapper/tests/Email_test.py
@@ -1,6 +1,34 @@
 import unittest
-from smtplibwrapper import Email
-from mock import MagicMock
+from smtplibwrapper import Email, EmailServer
+from mock import MagicMock, patch
+from smtplib import SMTPResponseException
+
+class TestEmailServer(unittest.TestCase):
+
+  def test_send(self):
+    email_server = EmailServer()
+    email = MagicMock()
+    email_server.server = MagicMock()
+
+    email.should_send.return_value = (False, "Email was empty")
+    (succeeded, reason) = email_server.send(email)
+    self.assertFalse(succeeded)
+    self.assertEqual(reason, "Email was empty")
+
+    email.should_send.return_value = (False, "Couldn't render email")
+    (succeeded, reason) = email_server.send(email)
+    self.assertFalse(succeeded)
+    self.assertEqual(reason, "Couldn't render email")
+
+    email.should_send.return_value = (True, "Seems OK to send")
+    (succeeded, reason) = email_server.send(email)
+    self.assertTrue(succeeded)
+    self.assertEqual(reason, "Sent OK")
+
+    email_server.server.sendmail.side_effect = SMTPResponseException(500, "Fake exception")
+    (succeeded, reason) = email_server.send(email)
+    self.assertFalse(succeeded)
+    self.assertEqual(reason, "Problem sending the email")
 
 class TestEmail(unittest.TestCase):
 
