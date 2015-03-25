f63e95c0a20409ebcd4fedbdee5b0bf67b154d21
Ben Taylor <bewt85@gmail.com>
2015-03-25 16:50:07 +0000

    Bump version

--- setup.py
+++ setup.py
@@ -7,7 +7,7 @@ def readme():
     return f.read()
 
 setup(name='jsontoemail',
-      version='0.0.1',
+      version='0.0.2',
       description='Takes a jinja2 template and some json and sends an email',
       long_description=readme(),
       url='https://github.com/sanger-pathogens/jsontoemail',
