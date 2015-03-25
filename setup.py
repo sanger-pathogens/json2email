cad346df3f88807bfb236d9c65cb7106c5a462e9
Ben Taylor <bewt85@gmail.com>
2015-03-25 11:15:03 +0000

    Add setup.py

---
+++ setup.py
@@ -0,0 +1,32 @@
+from setuptools import setup, find_packages
+import multiprocessing
+
+def readme():
+  with open(os.path.join(os.path.dirname(__file__), 'README.md')) as f:
+    return f.read()
+
+setup(name='jsontoemail',
+      version='0.0.1',
+      description='Takes a jinja2 template and some json and sends an email',
+      url='https://github.com/sanger-pathogens/jsontoemail',
+      author='Ben Taylor',
+      author_email='ben.taylor@sanger.ac.uk',
+      install_requires=[
+        'email'
+      ],
+      test_suite='nose.collector',
+      tests_require=[
+        'nose',
+        'mock'
+      ],
+      license='GPLv3',
+      packages=find_packages(),
+      classifiers=[
+        "License :: OSI Approved :: GNU General Public License (GPLv3)",
+        "Programming Language :: Python",
+        "Development Status :: 4 - Beta",
+        "Topic :: Communications :: Email",
+        "Topic :: System :: Monitoring",
+        "Topic :: Utilities"
+      ]
+)
